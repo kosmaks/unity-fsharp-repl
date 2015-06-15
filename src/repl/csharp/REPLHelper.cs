ed8193cff003c20d10817370b466187d2ffcb220
Maxim Kostuchenko <kstmaks@gmail.com>
2015-06-15 20:10:53 +0500

    Removed old eval loop

--- src/repl/csharp/REPLHelper.cs
+++ src/repl/csharp/REPLHelper.cs
@@ -7,22 +7,10 @@ using UnityFSharpRepl;
 
 [InitializeOnLoad]
 public class REPLHelper {
-    static readonly Queue<Action<object>> _queue = new Queue<Action<object>>();
-
     static REPLHelper() {
         var assembly = Assembly.GetExecutingAssembly().Location;
         REPL.run(assembly);
         EditorApplication.update -= REPL.update;
         EditorApplication.update += REPL.update;
-    }
-
-    static void Update() {
-        while (_queue.Count > 0) {
-            _queue.Dequeue()(null);
-        }
-    }
-
-    public static void RunOnMainThread(Action<object> action) {
-        _queue.Enqueue(action);
     }
 }
